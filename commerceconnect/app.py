2c40b66fbfc5230fd56e3d18f2919deec3c021ee
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-09-18 10:41:06 +0200

    Fixes CommerceConnectApplication so it can be useful for jeweettoch.

--- commerceconnect/app.py
+++ commerceconnect/app.py
@@ -1,12 +1,20 @@
+from oscar.core.loading import feature_hidden
+from oscar.views.decorators import permissions_required
+
 from commerceconnect.urls import urlpatterns
 from oscar.core.application import Application
 
 
 class CommerceConnectApplication(Application):
-    name = 'commerceconnect'
+    def get_url_decorator(self, pattern):
+        "Fixes bug in Application.get_url_decorator wich would decorate None values."
+        permissions = self.get_permissions(pattern.name)
+        if permissions is not None:
+            return permissions_required(permissions)
 
-    def get_urls(self):
+        return None
         
+    def get_urls(self):
         return self.post_process_urls(urlpatterns)
 
 
