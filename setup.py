e23976ec0a7462f08e362caff38dbc75edd30bf5
Martijn Jacobs <martijn@devopsconsulting.nl>
2014-12-02 12:42:02 +0100

    We need a 2.x.x series of the restframework

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         'setuptools',
         'django-oscar',
-        'djangorestframework'
+        'djangorestframework<3.0.0'
     ],
     # mark test target to require extras.
     extras_require={
