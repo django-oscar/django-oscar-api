1b571e2fddc2ea472e7f1aecc71384f9f8cf8cf8
Martijn Jacobs <martijn@devopsconsulting.nl>
2014-12-12 17:59:25 +0100

    We should make use of the strategy in the request, otherwise we get the wrong (default oscar) strategy

--- commerceconnect/apps/basket/utils.py
+++ commerceconnect/apps/basket/utils.py
@@ -5,9 +5,6 @@ __all__ = ('prepare_basket', 'get_basket', 'apply_offers', )
 
 
 Applicator = get_class('offer.utils', 'Applicator')
-Selector = get_class('partner.strategy', 'Selector')
-
-selector = Selector()
 
 
 def apply_offers(request, basket):
@@ -17,7 +14,7 @@ def apply_offers(request, basket):
 
 
 def prepare_basket(basket, request):
-    basket.strategy = selector.strategy(request=request, user=request.user)
+    basket.strategy = request.strategy
     apply_offers(request, basket)
 
     basket.store_basket(request)
@@ -27,7 +24,7 @@ def prepare_basket(basket, request):
 def get_basket(request, prepare=True):
     "Get basket from the request."
     Basket = get_model('basket', 'Basket')
-    
+
     if request.user.is_authenticated():
         basket = Basket.get_user_basket(request.user)
     else:
@@ -35,5 +32,4 @@ def get_basket(request, prepare=True):
         if basket is None:
             basket = Basket.editable.create()
             basket.save()
-
     return prepare_basket(basket, request) if prepare else basket
