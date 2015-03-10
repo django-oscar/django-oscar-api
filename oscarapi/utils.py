008d230442650f300fb8159add424163cc2d660c
Martijn Jacobs <martijn@devopsconsulting.nl>
2015-03-10 10:21:47 +0100

    Method is renamed in v3.0

--- oscarapi/utils.py
+++ oscarapi/utils.py
@@ -18,9 +18,10 @@ def overridable(name, default):
 
 
 class OscarSerializer(object):
-    field_mapping = dict(serializers.ModelSerializer.field_mapping, **{
-        oscar.models.fields.NullCharField: serializers.CharField
-    })
+    field_mapping = dict(
+        serializers.ModelSerializer.serializer_field_mapping, **{
+            oscar.models.fields.NullCharField: serializers.CharField
+        })
 
     def to_native(self, obj):
         num_fields = len(self.get_fields())
