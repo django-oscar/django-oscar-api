fdde13130a9b999af5cdac3a564f6d68b5890a81
Tom Hendrikx <tom@whyscream.net>
2016-08-26 15:09:19 +0200

    Use configured serializer in stead of the hardcoded one

--- oscarapi/views/login.py
+++ oscarapi/views/login.py
@@ -60,7 +60,7 @@ class LoginView(APIView):
         anonymous_basket.delete()
 
     def post(self, request, format=None):
-        ser = serializers.LoginSerializer(data=request.data)
+        ser = serializer_class(data=request.data)
         if ser.is_valid():
 
             anonymous_basket = operations.get_anonymous_basket(request)
