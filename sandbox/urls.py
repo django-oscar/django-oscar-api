75a2723e1d6fcef600f431d60f7d123be6cabdd9
Martijn Jacobs <martijn@devopsconsulting.nl>
2017-02-25 12:55:45 +0100

    'patterns' is deprecated since Django 1.8

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from oscar.app import application
 from oscarapi.app import application as api
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urls)),
     url(r'', include(application.urls)),
-)
+]
