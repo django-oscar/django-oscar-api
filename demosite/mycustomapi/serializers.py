0334e98bc780ca264b7f2b1d15df10e5b1992c10
Martijn Jacobs <martijn@devopsconsulting.nl>
2017-12-18 13:10:06 +0100

    Added example how to show the price in the ProductList view

--- demosite/mycustomapi/serializers.py
+++ demosite/mycustomapi/serializers.py
@@ -1,9 +1,28 @@
+from rest_framework import serializers
+
+from oscar.core.loading import get_class
+
+from oscarapi.serializers.checkout import PriceSerializer
 from oscarapi.serializers.product import (
-    ProductImageSerializer, ProductLinkSerializer)
+    ProductImageSerializer, ProductLinkSerializer
+)
+
+
+Selector = get_class('partner.strategy', 'Selector')
 
 
 class MyProductLinkSerializer(ProductLinkSerializer):
     images = ProductImageSerializer(many=True, required=False)
+    price = serializers.SerializerMethodField()
 
     class Meta(ProductLinkSerializer.Meta):
-        fields = ('url', 'id', 'title', 'images')
+        fields = ('url', 'id', 'title', 'images', 'price')
+
+    def get_price(self, obj):
+        request = self.context.get("request")
+        strategy = Selector().strategy(
+            request=request, user=request.user)
+        ser = PriceSerializer(
+            strategy.fetch_for_product(obj).price,
+            context={'request': request})
+        return ser.data
