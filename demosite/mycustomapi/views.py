dbed9556efa9d2850a34d359642ada4d1e54d5fd
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-01-18 10:47:41 +0100

    Fix import of the demosite

--- demosite/mycustomapi/views.py
+++ demosite/mycustomapi/views.py
@@ -1,7 +1,7 @@
-from oscarapi.views import basic
+from oscarapi.views import product
 
 from .serializers import MyProductLinkSerializer
 
 
-class ProductList(basic.ProductList):
+class ProductList(product.ProductList):
     serializer_class = MyProductLinkSerializer
