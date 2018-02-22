08def1158ba0d32f1b2c1313b6f09b8bae1c4e98
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-02-22 18:51:33 +0100

    Updated dependencies of the demosite

--- demosite/setup.py
+++ demosite/setup.py
@@ -23,7 +23,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'django-oscar-api',
-        'django>=1.9, <1.10',
-        'django-oscar>=1.3'
+        'django>=1.8, <2.0',
+        'django-oscar>=1.5.1'
     ],
 )
