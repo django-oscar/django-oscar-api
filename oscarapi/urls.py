7c08c0e4f44b9d44acd49c949b0ec804357270e4
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-05-13 11:52:34 +0200

    Split oscarapi in user and admin api

--- oscarapi/urls.py
+++ oscarapi/urls.py
@@ -103,24 +103,19 @@ urlpatterns = [
     url(r'^basket/add-product/$', AddProductView.as_view(), name='api-basket-add-product'),
     url(r'^basket/add-voucher/$', AddVoucherView.as_view(), name='api-basket-add-voucher'),
     url(r'^basket/shipping-methods/$', ShippingMethodView.as_view(), name='api-basket-shipping-methods'),
-    url(r'^baskets/$', BasketList.as_view(), name='basket-list'),
     url(r'^baskets/(?P<pk>[0-9]+)/$', BasketDetail.as_view(), name='basket-detail'),
     url(r'^baskets/(?P<pk>[0-9]+)/lines/$', LineList.as_view(), name='basket-lines-list'),
     url(r'^baskets/(?P<basket_pk>[0-9]+)/lines/(?P<pk>[0-9]+)/$', BasketLineDetail.as_view(), name='basket-line-detail'),
-    url(r'^lines/$', LineList.as_view(), name='line-list'),
     url(r'^lines/(?P<pk>[0-9]+)/$', LineDetail.as_view(), name='line-detail'),
-    url(r'^lineattributes/$', LineAttributeList.as_view(), name='lineattribute-list'),
     url(r'^lineattributes/(?P<pk>[0-9]+)/$', LineAttributeDetail.as_view(), name='lineattribute-detail'),
     url(r'^products/$', ProductList.as_view(), name='product-list'),
     url(r'^products/(?P<pk>[0-9]+)/$', ProductDetail.as_view(), name='product-detail'),
     url(r'^products/(?P<pk>[0-9]+)/price/$', ProductPrice.as_view(), name='product-price'),
     url(r'^products/(?P<pk>[0-9]+)/availability/$', ProductAvailability.as_view(), name='product-availability'),
     url(r'^products/(?P<pk>[0-9]+)/stockrecords/$', StockRecordList.as_view(), name='product-stockrecord-list'),
-    url(r'^stockrecords/$', StockRecordList.as_view(), name='stockrecord-list'),
     url(r'^stockrecords/(?P<pk>[0-9]+)/$', StockRecordDetail.as_view(), name='stockrecord-detail'),
     url(r'^options/$', OptionList.as_view(), name='option-list'),
     url(r'^options/(?P<pk>[0-9]+)/$', OptionDetail.as_view(), name='option-detail'),
-    url(r'^users/$', UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='user-detail'),
     url(r'^checkout/$', CheckoutView.as_view(), name='api-checkout'),
     url(r'^orders/$', OrderList.as_view(), name='order-list'),
@@ -130,10 +125,18 @@ urlpatterns = [
     url(r'^orderlineattributes/(?P<pk>[0-9]+)/$', OrderLineAttributeDetail.as_view(), name='order-lineattributes-detail'),
     url(r'^countries/$', CountryList.as_view(), name='country-list'),
     url(r'^countries/(?P<pk>[A-z]+)/$', CountryDetail.as_view(), name='country-detail'),
-    url(r'^partners/$', PartnerList.as_view(), name='partner-list'),
     url(r'^partners/(?P<pk>[0-9]+)/$', PartnerDetail.as_view(), name='partner-detail'),
-    url(r'^useraddresses/$', UserAddressList.as_view(), name='useraddress-list'),
     url(r'^useraddresses/(?P<pk>[0-9]+)/$', UserAddressDetail.as_view(), name='useraddress-detail')
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+admin_urlpatterns = [
+    url(r'^admin/baskets/$', BasketList.as_view(), name='basket-list'),
+    url(r'^admin/lines/$', LineList.as_view(), name='line-list'),
+    url(r'^admin/lineattributes/$', LineAttributeList.as_view(), name='lineattribute-list'),
+    url(r'^admin/stockrecords/$', StockRecordList.as_view(), name='stockrecord-list'),
+    url(r'^admin/users/$', UserList.as_view(), name='user-list'),
+    url(r'^admin/partners/$', PartnerList.as_view(), name='partner-list'),
+    url(r'^admin/useraddresses/$', UserAddressList.as_view(), name='useraddress-list'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns + admin_urlpatterns)
