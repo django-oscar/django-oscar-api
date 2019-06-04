39bc7bad2d094f1c8160be590f9f042cad5aa694
Martijn Jacobs <martijn@devopsconsulting.nl>
2019-06-04 16:01:00 +0200

    Added admin api order serializers

---
+++ oscarapi/serializers/admin/order.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from oscarapi.utils.loading import get_api_classes
+
+OrderSerializer, OrderLineSerializer, OrderLineAttributeSerializer = get_api_classes(  # pylint: disable=unbalanced-tuple-unpacking
+    "serializers.checkout",
+    [
+        "OrderSerializer",
+        "OrderLineSerializer",
+        "OrderLineAttributeSerializer"
+    ],
+)
+
+
+class AdminOrderSerializer(OrderSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='admin-order-detail')
+    lines = serializers.HyperlinkedIdentityField(
+        view_name='admin-order-lines-list')
+
+
+class AdminOrderLineAttributeSerializer(OrderLineAttributeSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='admin-order-lineattributes-detail')
+
+
+class AdminOrderLineSerializer(OrderLineSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='admin-order-lines-detail')
+    attributes = AdminOrderLineAttributeSerializer(
+        many=True, fields=('url', 'option', 'value'), required=False)
