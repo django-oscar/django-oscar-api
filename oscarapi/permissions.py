60462ee876d4ccd51196b24f5b2226d735e5f2e9
Martijn Jacobs <martijn@devopsconsulting.nl>
2019-06-05 17:01:10 +0200

    Update APIAdminPermission to respect view permissions

--- oscarapi/permissions.py
+++ oscarapi/permissions.py
@@ -1,4 +1,9 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated, DjangoModelPermissions
+from rest_framework.permissions import (
+    BasePermission,
+    IsAdminUser,
+    IsAuthenticated,
+    DjangoModelPermissions,
+)
 
 from oscarapi.basket.operations import request_allows_access_to
 
@@ -29,8 +34,24 @@ class IsOwner(IsAuthenticated):
 
 class APIAdminPermission(DjangoModelPermissions):
     """
-    The permission for all the admin api views, by default you get api
-    access if you've got django admin access.
-    
+    The permission for all the admin api views. You only get admin api access if you
+    are a staff user. You will get access to the objects according to the model
+    persmissons you have (iew / add / change / delete)
+
     Feel free to customize!
     """
+
+    perms_map = {
+        "GET": ["%(app_label)s.view_%(model_name)s"],
+        "OPTIONS": ["%(app_label)s.view_%(model_name)s"],
+        "HEAD": ["%(app_label)s.view_%(model_name)s"],
+        "POST": ["%(app_label)s.add_%(model_name)s"],
+        "PUT": ["%(app_label)s.change_%(model_name)s"],
+        "PATCH": ["%(app_label)s.change_%(model_name)s"],
+        "DELETE": ["%(app_label)s.delete_%(model_name)s"],
+    }
+
+    def has_permission(self, request, view):
+        if not request.user.is_staff:
+            return False
+        return super(APIAdminPermission, self).has_permission(request, view)
