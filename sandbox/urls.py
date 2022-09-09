922e4f5b5300e35bebd1a15f8b592ffa05604c0c
Martijn Jacobs <maerteijn@gmail.com>
2022-09-09 15:09:23 +0200

    Added a basic javascript login/fetch-basket example

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,10 +1,12 @@
 from django.apps import apps
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('api/', include('oscarapi.urls')),
+    path('js-login-example/', TemplateView.as_view(template_name='js-login-example.html')),
     path('', include(apps.get_app_config('oscar').urls[0]))
 ]
