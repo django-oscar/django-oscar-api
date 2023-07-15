736bc3a6cddeba59978c4f6f82a39a9497fb5633
Frank Hennige <frank@hennige-it.de>
2023-07-15 18:01:54 +0200

    Signal kwarg providing_args is Disallowed in Django 4.0

--- oscarapi/signals.py
+++ oscarapi/signals.py
@@ -1,3 +1,10 @@
+import django
 from django.dispatch import Signal
 
-oscarapi_post_checkout = Signal(providing_args=["order", "user", "request", "response"])
+
+oscarapi_post_checkout_args = ["order", "user", "request", "response"]
+
+if django.VERSION >= (3, 0):
+    oscarapi_post_checkout = Signal()
+else:
+    oscarapi_post_checkout = Signal(providing_args=oscarapi_post_checkout_args)
