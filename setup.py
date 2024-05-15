b49ca15fab2b4357dfb63181f925eb0d43bf82d9
Lars van de Kerkhof <lars@permanentmarkers.nl>
2024-05-15 14:54:38 +0200

    lal

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     ],
     # mark test target to require extras.
     extras_require={
-        "dev": ["coverage", "twine", "wheel", "sorl-thumbnail"],
+        "dev": ["coverage", "wheel", "easy_thumbnails"],
         "lint": ["pylint", "pylint-django", "black>=23.1.0"],
     },
 )
